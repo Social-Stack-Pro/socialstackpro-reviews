6a31cc2fc6ac2ed07b433a4ccdca9ccb213d3e57
daboat20 <76184306+daboat20@users.noreply.github.com>
2024-07-02 16:38:10 -0500

    Update get-reviews.js

--- functions/get-reviews.js
+++ functions/get-reviews.js
@@ -1,40 +1,36 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update({
+const s3 = new AWS.S3({
     accessKeyId: process.env.SSPReviews_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.SSPReviews_AWS_SECRET_ACCESS_KEY,
     region: 'us-east-1'
 });
 
-const s3 = new AWS.S3();
-
 exports.handler = async (event) => {
-    console.log('Received event:', event);
-    const bucketName = process.env.SSPReviews_BUCKET_NAME;
-    const key = 'reviews.json';
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const params = {
+        Bucket: process.env.SSPReviews_BUCKET_NAME,
+        Key: 'reviews.json'
+    };
 
     try {
-        const params = {
-            Bucket: bucketName,
-            Key: key,
-        };
         console.log('Fetching reviews with params:', params);
-
         const data = await s3.getObject(params).promise();
-        console.log('Raw data fetched:', data);
-
         const reviews = JSON.parse(data.Body.toString('utf-8'));
+        console.log('Raw data fetched:', data);
         console.log('Parsed reviews:', reviews);
 
+        // Ensure reviews is an array
         return {
             statusCode: 200,
-            body: JSON.stringify(reviews),
+            body: JSON.stringify(Array.isArray(reviews) ? reviews : [reviews])
         };
     } catch (error) {
         console.error('Error fetching reviews:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch reviews' }),
+            body: JSON.stringify({ error: 'Failed to fetch reviews' })
         };
     }
 };
